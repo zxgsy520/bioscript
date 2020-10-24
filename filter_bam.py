3a9f7019c3982b646f3d042b0d16c38b9842f78a
sy520 <36355222+zxgsy520@users.noreply.github.com>
2020-10-24 19:22:05 +0800

    Add files via upload

--- filter_bam.py
+++ filter_bam.py
@@ -8,57 +8,46 @@ import pysam
 import logging
 import argparse
 
-
 LOG = logging.getLogger(__name__)
 
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 __author__ = ("Xingguo Zhang",)
 __email__ = "113178210@qq.com"
 __all__ = []
 
 
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        pass
-    try:
-        import unicodedata
-        unicodedata.numeric(s)
-        return True
-    except (TypeError, ValueError):
-        pass
-    return False
-
-
 def gmk2pb(string):
 
-    string = string.strip().lower()
-    size = 0
+    string = string.lower().strip()
 
-    if string.endswith('gb') or string.endswith('g'):
-        string = string.split('g')[0]
-        size = float(string)*1e9
-    elif string.endswith('mb') or string.endswith('m'):
-        string = string.split('m')[0]
-        size = float(string)*1e6
-    elif string.endswith('kb') or string.endswith('k'):
-        string = string.split('k')[0]
-        size = float(string)*1e3
-    elif string.endswith('bp') or string.endswith('b') or is_number(string):
-        string = string.split('b')[0]
-        size = float(string)
+    if string.endswith('g') or string.endswith('gb'):
+        base = string.split('g')[0]
+        base = float(base)*1e+09
+    elif string.endswith('m') or string.endswith('mb'):
+        base = string.split('m')[0]
+        base = float(base)*1e+06
+    elif string.endswith('k') or string.endswith('kb'):
+        base = string.split('k')[0]
+        base = float(base)*1e+03
     else:
-        pass
-#        raise Exception("%s format error" % string)
+        base = float(string)
 
-    return size
+    return int(base)
 
 
-def filter_bam(file, ofile, minlen=500, minqv=0.8, tbase="500Mb"):
+def get_sample(file):
 
-    tbase = gmk2pb(tbase)
+    name = file.split('/')[-1]
+
+    if '--' in name:
+        name = name.split('--')[1].split('.bam')[0]
+    else:
+        name = name.split('.')[0]
+
+    return name
+
+
+def filter_bam(file, qvalue=0.8, minlen=500, keep='1Gb'):
 
     if file.endswith(".bam"):
         fh = pysam.AlignmentFile(file, "rb", check_sq=False)
@@ -67,35 +56,44 @@ def filter_bam(file, ofile, minlen=500, minqv=0.8, tbase="500Mb"):
     else:
         raise Exception("%r file format error" % file)
 
-    fo = pysam.AlignmentFile(ofile, "wb", template=fh)
+    name = get_sample(file)
+    fo = pysam.AlignmentFile("%s.clean.bam" % name, "wb", template=fh)
     n = 0
-    print(tbase)
 
     for line in fh:
-        if line.get_tag('rq')<minqv or len(line.seq)<minlen:
+        if line.get_tag('rq') <= qvalue:
             continue
-        n += len(line.seq)
-        fo.write(line)
-        
-        if tbase and n>=tbase:
+        if len(line.seq)<= minlen:
+            continue
+        if keep=="" or keep=="all":
+            pass
+        elif gmk2pb(keep) <= n:
             break
+        else:
+            pass
+        fo.write(line)
+        n += len(line.seq)
 
     fh.close()
     fo.close()
 
 
+def filter_bams(files, qvalue, minlen, keep):
+
+    for file in files:
+        filter_bam(file, qvalue, minlen, keep)
+
+
 def add_hlep_args(parser):
 
-    parser.add_argument('bam',
+    parser.add_argument('-i', '--input', nargs='+', metavar='FILE', type=str, required=True,
         help='Input reads file, format(bam and sam).')
-    parser.add_argument('-l', '--minlen', metavar='INT', type=int, default=500,
-        help='Minimum read length for filtering, default=500.')
-    parser.add_argument('-q', '--minqv', metavar='FLOAT', type=float, default=0.8,
-        help='Filtered minimum read quality value, default=0.8.')
-    parser.add_argument('-b', '--base', metavar='STR', type=str, default='500Mb',
-        help='Number of bases reserved, default=500Mb.')
-    parser.add_argument('-o', '--out', metavar='STR', type=str, default='out.bam',
-        help='Output result file,default=out.bam.')
+    parser.add_argument('-q', '--qvalue', metavar='FLOAT', type=float, default=0.8,
+        help='Input filtered quality value, default=0.8.')
+    parser.add_argument('-l', '--minlen', metavar='FLOAT', type=float, default=500,
+        help='Input the minimum read length for filtering, default=500.')
+    parser.add_argument('-k', '--keep', metavar='STR', type=str, default='all',
+        help='Number of bases reserved, default=all.')
 
     return parser
 
@@ -110,17 +108,17 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
     description='''
 name:
-    filter_bam.py Filter the bam files sequenced by pacbio.
+    filter_bam.py Filter bam files based on quality values.
 
 attention:
-    filter_bam.py input.bam -o out.bam
+    filter_bam.py -i *.bam
 version: %s
 contact:  %s <%s>\
         ''' % (__version__, ' '.join(__author__), __email__))
 
     args = add_hlep_args(parser).parse_args()
 
-    filter_bam(args.bam, args.out, args.minlen, args.minqv, args.base)
+    filter_bams(args.input, args.qvalue, args.minlen, args.keep)
 
 
 if __name__ == "__main__":
